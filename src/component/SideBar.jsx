c46a2fa1cceea23b663727edf6e3e5f6923e181b
Chetan <chetan.satpute2002@gmail.com>
2020-11-25 15:25:56 +0530

    Added fade transition on opening sidebar (on mobile device)

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -2,10 +2,54 @@ import React from 'react';
 import './css/sideBar.css';
 
 class SideBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            childRender: false
+        }
+    }
+
+    componentDidMount() {
+        if (window.screen.width <= 800)
+        {
+            var value = 0;
+
+            var sideBar = document.getElementById('sideBar');
+
+            var interval = setInterval(() => {
+                value += 0.01;
+
+                sideBar.style.opacity = value;
+
+                this.render();
+
+                if (value > 1)
+                {
+                    clearInterval(interval);
+                    this.setState({ childRender: true });
+                }
+
+            }, 1);
+        } else {
+            this.setState({ childRender: true });
+        }
+    }
 
     render() {
+
         return (
-            <div id="sideBar"> This is sidebar.</div>
+            <div id="sideBar"> 
+            
+            {this.state.childRender && 
+            
+                <React.Fragment>
+                    
+                    This is sidebar.
+
+                </React.Fragment> }
+            
+            </div>
         );
     }
 }

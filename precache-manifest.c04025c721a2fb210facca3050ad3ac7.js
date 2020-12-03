8aeae4f915befc9d4dc6dce67e4d658d41f57e93
Chetan <chetan.satpute2002@gmail.com>
2020-12-03 22:29:17 +0530

    Updates

---
+++ precache-manifest.c04025c721a2fb210facca3050ad3ac7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442a79d4c5e6ccc849032835235f8ac1",
+    "url": "/oneline/index.html"
+  },
+  {
+    "revision": "7ca55f5175a8deee450d",
+    "url": "/oneline/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "6b32b376cb0eed793067",
+    "url": "/oneline/static/css/main.236e4a6f.chunk.css"
+  },
+  {
+    "revision": "7ca55f5175a8deee450d",
+    "url": "/oneline/static/js/2.a0b4e1d0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/oneline/static/js/2.a0b4e1d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b32b376cb0eed793067",
+    "url": "/oneline/static/js/main.5742fd52.chunk.js"
+  },
+  {
+    "revision": "04a9fa57b1cd26122121",
+    "url": "/oneline/static/js/runtime-main.4a8ae461.js"
+  },
+  {
+    "revision": "1e968d6528f9d3c0baec6715faf290a0",
+    "url": "/oneline/static/media/cursor.1e968d65.svg"
+  },
+  {
+    "revision": "9b64f44306ad57e05e912348a442ee7f",
+    "url": "/oneline/static/media/eraser.9b64f443.svg"
+  },
+  {
+    "revision": "12a68a37ce0b3ee5d2f248b07f5e6916",
+    "url": "/oneline/static/media/play.12a68a37.svg"
+  },
+  {
+    "revision": "e7ae5b9569ac79f335a1e54b99afec64",
+    "url": "/oneline/static/media/stop.e7ae5b95.svg"
+  }
+]);
